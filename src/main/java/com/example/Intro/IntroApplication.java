a755cf069c054f001bb90f01293dfc754c52eaf2
AnthonyUPF <anthony.torres.h@gmail.com>
2023-01-26 12:23:46 +0100

    first commit

--- src/main/java/com/example/Intro/IntroApplication.java
+++ src/main/java/com/example/Intro/IntroApplication.java
@@ -1,17 +1,16 @@
 package com.example.Intro;
 
-import com.example.Intro.models.Aircraft;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
+
 
 @SpringBootApplication
 public class IntroApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntroApplication.class, args);
-
 
 	}
 
